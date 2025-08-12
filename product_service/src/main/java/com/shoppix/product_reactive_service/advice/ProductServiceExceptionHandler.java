51679b7a92dcc422cea26bfd207543f8458aaa8d
dev-kartik4 <kartiktelikicherla@outlook.com>
2025-08-12 23:04:04 +0530

    ECOM APP CHANGES
    ECOM APP CHANGES

--- product_service/src/main/java/com/shoppix/product_reactive_service/advice/ProductServiceExceptionHandler.java
+++ product_service/src/main/java/com/shoppix/product_reactive_service/advice/ProductServiceExceptionHandler.java
@@ -1,7 +1,7 @@
 package com.shoppix.product_reactive_service.advice;
 
 import com.shoppix.product_reactive_service.exception.ProductServiceException;
-import com.shoppix.product_reactive_service.pojo.ResponseErrorMessage;
+import com.shoppix.product_reactive_service.pojo.ResponseMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,12 +13,12 @@ public class ProductServiceExceptionHandler {
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(ProductServiceException.class)
-    public ResponseEntity<ResponseErrorMessage> handleProductServiceException(ProductServiceException productEx){
+    public ResponseEntity<ResponseMessage> handleProductServiceException(ProductServiceException productEx){
 
-        ResponseErrorMessage responseErrorMessage = new ResponseErrorMessage();
-        responseErrorMessage.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
-        responseErrorMessage.setMessage(productEx.getMessage());
+        ResponseMessage responseMessage = new ResponseMessage();
+        responseMessage.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        responseMessage.setMessage(productEx.getMessage());
         productEx.printStackTrace();
-        return new ResponseEntity(responseErrorMessage, HttpStatus.valueOf(responseErrorMessage.getStatusCode()));
+        return new ResponseEntity(responseMessage, HttpStatus.valueOf(responseMessage.getStatusCode()));
     }
 }
---
+++ product_service/src/main/java/com/shoppix/product_reactive_service/enums/MerchantEnum.java
@@ -0,0 +1,8 @@
+package com.shoppix.product_reactive_service.enums;
+
+public enum MerchantEnum {
+
+    MERCHANT_REGISTERED,
+    MERCHANT_UPDATED,
+    MERCHANT_DELETED,
+}
