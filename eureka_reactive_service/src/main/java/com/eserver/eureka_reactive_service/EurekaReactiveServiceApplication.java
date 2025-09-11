fb8cec356bc738b343d3cf424a7e5d05be4e3393
Kartik Telikicherla <76726711+dev-kartik4@users.noreply.github.com>
2025-09-12 01:09:04 +0530

    LOG CHECK

--- eureka_reactive_service/src/main/java/com/eserver/eureka_reactive_service/EurekaReactiveServiceApplication.java
+++ eureka_reactive_service/src/main/java/com/eserver/eureka_reactive_service/EurekaReactiveServiceApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaReactiveServiceApplication {
 
 	public static void main(String[] args) {
+        System.out.println("HEY BUDDY EUREKA IS UP ! CHEERS");
 		SpringApplication.run(EurekaReactiveServiceApplication.class, args);
 	}
 
